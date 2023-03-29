f110fea14575269998aded11e477c829e5adf58e
Mohammad Ali <muhammad.ali@shayansolutions.com>
2023-03-29 16:24:49 +0500

    for css and jsx

---
+++ src/random.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Random = () => {
+  return (
+    <div>
+      Random
+      <div>this div belongs to sir arsalan</div>
+      <span>this span is mine</span>
+      <div>dragon-ball z</div>
+    </div>
+  );
+};
+
+export default Random;
